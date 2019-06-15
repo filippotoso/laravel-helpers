00cd913d4483c59f465a5204e96ad5b11930aca3
Filippo Toso <filippo.toso@creativepark.it>
2019-06-15 15:34:11 +0200

    Add human_filesize() helper

--- src/Helpers.php
+++ src/Helpers.php
@@ -11,3 +11,11 @@ function field(array $data)
 {
     return new FilippoToso\LaravelHelpers\UI\Field($data);
 }
+
+function human_filesize($bytes, $decimals = 2, $decimalSeparator = null, $thousandSeparator = null) {
+    $decimalSeparator = $decimalSeparator ?? (localeconv()['decimal_point'] ?? '.');
+    $thousandSeparator = $thousandSeparator ?? (localeconv()['thousands_sep'] ?? ',');
+    $size = array('B','KB','MB','GB','TB','PB','EB','ZB','YB');
+    $factor = min(floor((strlen($bytes) - 1) / 3), count($size) - 1);
+    return number_format($bytes / pow(1024, $factor), $decimals, $decimalSeparator, $thousandSeparator) . (isset($size[$factor]) ? $size[$factor] : end($size[$factor]));
+}
