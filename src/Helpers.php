114685a9b75a93b8958bc575ed31adc8184829ac
Filippo Toso <filippo.toso@creativepark.it>
2019-06-16 22:36:42 +0200

    Improve documentation

--- src/Helpers.php
+++ src/Helpers.php
@@ -12,10 +12,21 @@ function field(array $data)
     return new FilippoToso\LaravelHelpers\UI\Field($data);
 }
 
-function human_filesize($bytes, $decimals = 2, $decimalSeparator = null, $thousandSeparator = null) {
+/**
+ * Return the file size in human readable format
+ *
+ * @param  integer $bytes
+ * @param  integer $decimals
+ * @param  string|null $decimalSeparator
+ * @param  string|null $thousandSeparator
+ *
+ * @return string
+ */
+function human_filesize($bytes, $decimals = 2, $decimalSeparator = null, $thousandSeparator = null)
+{
     $decimalSeparator = $decimalSeparator ?? (localeconv()['decimal_point'] ?? '.');
     $thousandSeparator = $thousandSeparator ?? (localeconv()['thousands_sep'] ?? ',');
-    $size = array('B','KB','MB','GB','TB','PB','EB','ZB','YB');
+    $size = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
     $factor = min(floor((strlen($bytes) - 1) / 3), count($size) - 1);
-    return number_format($bytes / pow(1024, $factor), $decimals, $decimalSeparator, $thousandSeparator) . (isset($size[$factor]) ? $size[$factor] : end($size[$factor]));
+    return number_format($bytes / pow(1024, $factor), $decimals, $decimalSeparator, $thousandSeparator) . $size[$factor];
 }
