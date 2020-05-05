4526968107c1e0b0e0175736eb9d6bca31564385
Filippo Toso <filippo@toso.dev>
2020-05-05 14:27:07 +0200

    Add resources

---
+++ src/Resources/Resource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource as BaseResource;
+use Illuminate\Database\Eloquent\Collection;
+
+class Resource extends BaseResource
+{
+
+    protected function include($relationship, $resourceClass, $default = [], $flat = false)
+    {
+        if ($this->relationLoaded($relationship)) {
+            if (is_a($this->$relationship, Collection::class)) {
+                if ($flat) {
+                    return new $resourceClass($this->$relationship->first());
+                }
+                return new UnwrappedResourceCollection($this->$relationship, $resourceClass);
+            }
+            return new $resourceClass($this->$relationship);
+        }
+
+        return $default;
+    }
+
+}
